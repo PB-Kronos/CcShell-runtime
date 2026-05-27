e5792dcd58c9aba9b247b17aa4b450fe3457b966
PB-Kronos <sandervdmolen2010@gmail.com>
2026-05-27 18:53:48 +0200

    Update install.lua

--- pkg/execute/install.lua
+++ pkg/execute/install.lua
@@ -1,8 +1,8 @@
 local function down()
     
-    shell.run("wget https://github.com/PB-Kronos/CcShell-runtime/main/pkg/execute/execute.lua /home/download/execute.lua")
+    shell.run("wget https://raw.githubusercontent.com/PB-Kronos/CcShell-runtime/main/pkg/execute/execute.lua /home/download/execute.lua")
 end
 local function install()
-    shell.run("wget https://github.com/PB-Kronos/CcShell-runtime/main/pkg/execute/execute.lua /bin/execute.lua")
+    shell.run("wget https://raw.githubusercontent.com/PB-Kronos/CcShell-runtime/main/pkg/execute/execute.lua /bin/execute.lua")
 end
 if downloader then down() elseif downloader == false then install() else error("download is nil") end
